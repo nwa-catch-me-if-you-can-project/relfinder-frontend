ba583cffa53e0081879a124b02aab7840f1f5592
Walter Simoncini <walter@ashita.nl>
2021-08-09 16:44:06 +0200

    Basic API integration

--- src/main.js
+++ src/main.js
@@ -1,16 +1,33 @@
 import Vue from 'vue'
+
 import App from './App.vue'
+
+// Polyfill for older browsers. The import must
+// be before any Vuex usage
+import 'es6-promise/auto'
+import Vuex from 'vuex'
 
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 
 import router from './router'
+const axios = require('axios')
 
 Vue.config.productionTip = false;
 
 Vue.use(Buefy);
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    api: axios.create({
+      baseURL: process.env.VUE_APP_API_ROOT
+    }),
+  }
+})
 
 new Vue({
   router,
   render: h => h(App),
+  store: store
 }).$mount('#app');
--- src/views/GraphBrowser.vue
+++ src/views/GraphBrowser.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container is-fluid app-container">
         <div class="columns">
-            <div class="column is-3 app-sidebar-col">
+            <div class="column is-4 is-5-tablet is-3-fullhd app-sidebar-col">
                 <nav class="panel app-sidebar">
                     <p class="panel-heading">
                         UM-IDS RelFinder
@@ -9,8 +9,8 @@
                     <div class="panel-block">
                         <b-field label="Select Entities" class="entities-tag-input">
                             <b-taginput
-                                v-model="selectedEntities"
-                                :data="filteredEntities"
+                                v-model="selectedEntityTags"
+                                :data="filteredEntityTags"
                                 autocomplete
                                 icon="label"
                                 :open-on-focus="true"
@@ -28,7 +28,7 @@
                             <div class="columns">
                                 <div class="column">
                                     <b-button
-                                        @click="refreshGraph"
+                                        @click="executeQuery"
                                         type="is-primary refresh-btn">
                                         Refresh Graph
                                     </b-button>
@@ -44,15 +44,50 @@
                             </div>
                         </div>
                     </div>
+
+                    <div
+                        v-if="selectedEntity"
+                        class="property-table-container">
+                        <h3 class="subtitle">Entity properties</h3>
+                        <p>
+                            Data properties of entity
+                            <b>{{ selectedEntity.label }}</b>
+                            of class
+                            <b>{{ selectedEntity.class }}</b>
+                            with IRI
+                            <b>{{ selectedEntity.iri }}</b>
+                        </p>
+
+                        <br/>
+
+                        <b-table
+                            :data="entityTableData"
+                            :columns="entityTableColumns">
+                        </b-table>
+                    </div>
                 </nav>
             </div>
             <div class="column rel-network-col">
+                <svg style="height: 0vh; position: absolute">
+                    <defs>
+                        <marker id="m-end" markerWidth="10" markerHeight="10" refX="19" refY="3" orient="auto" markerUnits="strokeWidth" >
+                            <path d="M0,0 L0,6 L9,3 z"></path>
+                        </marker>
+                    </defs>
+                </svg>
+
+                <p v-if="!graphVisible" class="graph-info-label">Select two or more entities and press <b>Refresh Graph</b> to begin</p>
+
                 <d3-network
+                    v-if="graphVisible"
                     class="rel-network"
                     ref="relationship-network"
                     :net-nodes="this.graph.nodes"
                     :net-links="this.graph.links"
-                    :options="graphOptions" />
+                    :options="graphOptions"
+                    :link-cb="customizeLink"
+                    @link-click="getLinkProperties"
+                    @node-click="getEntityDataProperties" />
             </div>
         </div>
     </div>
@@ -60,8 +95,9 @@
 
 <script>
 
+import Values from 'values.js'
 import D3Network from 'vue-d3-network'
-import mockGraph from '@/../graph/demo-graph.json'
+var randomColor = require('randomcolor')
 
 export default {
     name: "GraphBrowser",
@@ -71,63 +107,213 @@ export default {
     data: function () {
         return {
             entities: [],
-            filteredEntities: [],
-            selectedEntities: [],
+            entityTags: [],
+            filteredEntityTags: [],
+            selectedEntityTags: [],
             graph: {
                 nodes: [],
                 links: []
             },
             graphOptions: {
-                nodeSize: 45,
+                nodeSize: 50,
                 nodeLabels: true,
-                linkLabels:true,
-                force: 4500
-            }
+                linkLabels: true,
+                force: 25000,
+                linkWidth: 2,
+                strLinks: true
+            },
+            selectedEntity: null,
+            entityTableData: [],
+            entityTableColumns: [
+                {
+                    field: "property",
+                    label: "Property"
+                },
+                {
+                    field: "value",
+                    label: "Value",
+                    // Needed to align the cell to the right
+                    numeric: true
+                }
+            ]
+        }
+    },
+    computed: {
+        graphVisible: function () {
+            return this.graph.nodes.length > 0 && this.graph.links.length > 0;
         }
     },
     methods: {
         getFilteredTags (text) {
-            this.filteredEntities = this.entities.filter((option) => {
+            this.filteredEntityTags = this.entityTags.filter((option) => {
                 // The option must not have been chosen yet and match the partial
                 // text typed in the input field
-                return !this.selectedEntities.includes(option) && option.toLowerCase().indexOf(text.toLowerCase()) >= 0
+                return !this.selectedEntityTags.includes(option) && option.toLowerCase().indexOf(text.toLowerCase()) >= 0
             })
         },
         tagListEdited () {
             // When a tag is added/removed filter the allowable tags
             // to the ones not chosen yet
-            this.filteredEntities = this.entities.filter((option) => {
-                return !this.selectedEntities.includes(option)
+            this.filteredEntityTags = this.entityTags.filter((option) => {
+                return !this.selectedEntityTags.includes(option)
             });
         },
-        refreshGraph () {
-            let filteredNodes = mockGraph.nodes.filter(node => this.selectedEntities.includes(node.name));
-            let filteredNodesIDs = filteredNodes.map(node => node.id);
-            let filteredEdges = mockGraph.edges.filter(edge => filteredNodesIDs.includes(edge.sid) && filteredNodesIDs.includes(edge.tid));
-
-            this.graph.nodes = filteredNodes;
-            this.graph.links = filteredEdges;
-        },
         resetFilters () {
-            this.graph.nodes = mockGraph.nodes;
-            this.graph.links = mockGraph.edges;
+            this.selectedEntityTags = [];
+            this.graph.nodes = [];
+            this.graph.links = [];
         },
-        entitiesFromGraph(graph) {
-            return graph.nodes.map(x => x.name);
+        refreshEntityTags (entities) {
+            let entityTags = [];
+
+            for (let i = 0; i < entities.length; i++) {
+                let iri = entities[i].iri.replaceAll(
+                    process.env.VUE_APP_KG_PREFIX,
+                    ""
+                )
+
+                entityTags.push(`${entities[i].label} (${iri})`);
+            }
+
+            this.entityTags = entityTags;
+            this.filteredEntityTags = entityTags;
+        },
+        fetchEntities () {
+            let view = this;
+
+            this.$store.state.api.get("entities").then(response => {
+                view.entities = response.data.entities;
+                view.refreshEntityTags(view.entities);
+            }).catch((err) => {
+                console.log(err);
+
+                view.$buefy.toast.open({
+                    message: "Could not fetch entities. Please reload the page",
+                    type: "is-danger"
+                })
+            })
+        },
+        customizeLink (link) {
+            link._svgAttrs = {
+                "marker-end": "url(#m-end)",
+            }
+
+            return link
+        },
+        getEntityDataProperties (event, node) {
+            let view = this;
+            this.deselectNodes();
+
+            const nodeColor = new Values(node._color);
+
+            node._cssClass = "selected-node";
+            node._svgAttrs.stroke = nodeColor.shade(12).hexString();
+
+            this.selectedEntity = node;
+
+            console.log(node);
+
+            this.$store.state.api.post("entities/properties", {iri: node.iri}).then(response => {
+                console.log(response.data);
+
+                let propertyData = [];
+
+                response.data.forEach(prop => {
+                    propertyData.push({
+                        property: prop.label,
+                        value: prop.value
+                    });
+                });
+
+                view.entityTableData = propertyData;
+            }).catch((err) => {
+                console.log(err);
+
+                view.$buefy.toast.open({
+                    message: "Could not fetch the entity properties. Please reload the page",
+                    type: "is-danger"
+                })
+            })
+        },
+        getLinkProperties (event, link) {
+            console.log(link);
+            link._svgAttrs.stroke = "#000";
+        },
+        deselectNodes () {
+            // Removes selection markers
+            this.graph.nodes.forEach(n => {
+                n._svgAttrs.stroke = null;
+            })
+        },
+        executeQuery () {
+            let view = this;
+            let entityIRIs = [];
+
+            for (let i = 0; i < this.selectedEntityTags.length; i++) {
+                // Extract the last string between parenthesis in the tag name
+                let parenthesisMatches = this.selectedEntityTags[i].match(/\(([^)]+)\)/)
+                let entityId = parenthesisMatches[parenthesisMatches.length - 1];
+
+                // Add the IRI prefix
+                let iri = `${process.env.VUE_APP_KG_PREFIX}${entityId}`
+
+                entityIRIs.push(iri);
+            }
+
+            this.$store.state.api.post("query", {entities: entityIRIs}).then(response => {
+                console.log(response.data)
+
+                response.data.nodes.forEach(n => {
+                    n.name = n.label;
+                    n._svgAttrs = {};
+                });
+
+                response.data.edges.forEach(e => {
+                    e._color = "#000000";
+                    e.name = e.label;
+                });
+
+                view.graph.nodes = response.data.nodes;
+                view.graph.links = response.data.edges;
+
+                // Assign colors to nodes
+                let classColorDictionary = {};
+
+                response.data.classes.forEach(graphClass => {
+                    classColorDictionary[graphClass] = randomColor({
+                        luminosity: "light"
+                    });
+                })
+
+                view.graph.nodes.forEach(n => {
+                    n._color = classColorDictionary[n.class]
+                })
+
+                // Dirty trick to make link labels visible.
+                // No method is exposed by the component to
+                // monitor graph updates
+                setTimeout(view.updateLinkLabels, 500);
+            }).catch((err) => {
+                console.log(err);
+
+                view.$buefy.toast.open({
+                    message: "Could not execute the query. Please retry later",
+                    type: "is-danger"
+                })
+            })
+        },
+        updateLinkLabels () {
+            // Shift back the arrow labels to avoid them
+            // being covered by the nodes
+            let textPaths = document.getElementsByTagName("textPath");
+
+            for (let i = 0; i < textPaths.length; i++) {
+                textPaths[i].setAttribute("startOffset", "10%");
+            }
         }
     },
-    mounted: function () {
-        this.classesData = {};
-        mockGraph.classes.forEach(clsObject => (this.classesData[clsObject.class] = { color: clsObject.color }));
-
-        this.graph.nodes = mockGraph.nodes;
-        this.graph.links = mockGraph.edges;
-
-        this.entities = this.entitiesFromGraph(this.graph);
-
-        this.graph.nodes.forEach(n => (
-            n._color = this.classesData[n.class].color
-        ));
+    created: function () {
+        this.fetchEntities();
     }
 };
 
@@ -188,6 +374,48 @@ export default {
 
 .buttons-container > .columns {
     padding-right: 0.75rem;
+}
+
+.graph-info-label {
+    top: 40%;
+    position: relative;
+    text-align: center;
+}
+
+#m-end path {
+  fill: #E5E5E5;
+}
+
+.property-table-container {
+    margin-top: 16px;
+    margin-left: 16px;
+    margin-right: 16px;
+}
+
+</style>
+
+<style>
+
+.link {
+    stroke: #E5E5E5 !important;
+}
+
+.node-label {
+    font-weight: bold;
+    font-size: 12px;
+}
+
+textPath {
+    font-size: 12px;
+}
+
+.link-label {
+    transform: translate(0, -16px);
+    z-index: 100;
+}
+
+.selected-node {
+    stroke-width: 5;
 }
 
 </style>
